7af82726e706fa843f0b309c0a7e487448f7a8bc
Emmanuel-Olabanji <timtim2k10@gmail.com>
2022-03-24 03:34:21 +0000

    api 5 day

--- firstv/src/components/homepage.js
+++ firstv/src/components/homepage.js
@@ -46,7 +46,37 @@ export class Mainpage extends React.Component
           highTemp: null,
           lowTemp: null,
           
-          visibility: null
+          visibility: null,
+
+
+
+          day1 : null ,
+          day1temp : null,
+          day1img : null ,
+
+
+          day2 : null ,
+          day2temp : null,
+          day2img : null ,
+
+
+
+          day3 : null ,
+          day3temp : null,
+          day3img : null ,
+
+
+
+          day4 : null ,
+          day4temp : null,
+          day4img : null ,
+
+
+
+          day5: null ,
+          day5temp : null,
+          day5adimg : null 
+
 
       };
   }
@@ -54,7 +84,9 @@ export class Mainpage extends React.Component
 
 
   async componentDidMount() {
-      const url = "https://api.openweathermap.org/data/2.5/weather?zip=W8,GB&appid=5afc51e761e2e7eaa9e64dada883952c";
+      //const url = "https://api.openweathermap.org/data/2.5/weather?zip=W8,GB&appid=5afc51e761e2e7eaa9e64dada883952c";
+      const url = "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=43980dc2ea135942958ac549b841ea53";
+      
       const response = await fetch(url);
       const data = await response.json();
       var d = new Date();
@@ -63,35 +95,35 @@ export class Mainpage extends React.Component
       
 
       this.setState({date: date});
-      this.setState({city: data.name});
-      this.setState({country: data.sys.country});
-      this.setState({humidity: data.main.humidity});
-      this.setState({windSpeed: data.wind.speed});
+      this.setState({city: data.city.name});
+      this.setState({country: data.city.country});
+      this.setState({humidity: data.list[0].main.humidity});
+      this.setState({windSpeed: data.list[0].wind.speed});
 
-      this.setState({weatherIcon: ("http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png")});
-      this.setState({weatherDesc: data.weather[0].main});
-      this.setState({feelsLikeTemp: Math.round(data.main.feels_like-273.15)}); // IN CELSIUS
-      this.setState({highTemp: Math.round(data.main.temp_max-273.15)}); // IN CELSIUS
-      this.setState({lowTemp: Math.round(data.main.temp_min-273.15)}); // IN CELSIUS
+      this.setState({weatherIcon: ("http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png")});
+      this.setState({weatherDesc: data.list[0].weather[0].main});
+      this.setState({feelsLikeTemp: Math.round(data.list[0].main.feels_like-273.15)}); // IN CELSIUS
+      this.setState({highTemp: Math.round(data.list[0].main.temp_max-273.15)}); // IN CELSIUS
+      this.setState({lowTemp: Math.round(data.list[0].main.temp_min-273.15)}); // IN CELSIUS
 
-      this.setState({feelsLikeTempF: Math.round((data.main.feels_like*1.8)-459.67)}); // IN FAHRENHEIT
-      this.setState({highTempF: Math.round((data.main.temp_max*1.8)-459.67)}); // IN FAHRENHEIT
-      this.setState({lowTempF: Math.round((data.main.temp_min*1.8)-459.67)}); // IN FAHRENHEIT
+      this.setState({feelsLikeTempF: Math.round((data.list[0].main.feels_like*1.8)-459.67)}); // IN FAHRENHEIT
+      this.setState({highTempF: Math.round((data.list[0].main.temp_max*1.8)-459.67)}); // IN FAHRENHEIT
+      this.setState({lowTempF: Math.round((data.list[0].main.temp_min*1.8)-459.67)}); // IN FAHRENHEIT
       
       
-     if(data.main.feels_like>293.15 && data.main.feels_like<303.15)
+     if(data.list[0].main.feels_like>293.15 && data.list[0].main.feels_like<303.15)
       {
          this.setState({sport1: marathoning});
          this.setState({sport2: hiking});
          this.setState({sport3: cycling});
       }
-      else if(data.main.feels_like>278.15 && data.main.feels_like<293.15)
+      else if(data.list[0].main.feels_like>278.15 && data.list[0].main.feels_like<293.15)
       {
          this.setState({sport1: weight});
          this.setState({sport2: skateboarding});
          this.setState({sport3: running});
       }
-      else if(data.main.feels_like>268.15 && data.main.feels_like<278.15)
+      else if(data.list[0].main.feels_like>268.15 && data.list[0].main.feels_like<278.15)
       {
          this.setState({sport1: indoorrunning});
          this.setState({sport2: skiing});
@@ -101,8 +133,69 @@ export class Mainpage extends React.Component
       console.log(data);
       console.log("Weather icon:", this.state.weatherIcon);
 
-      this.setState({visibility: data.visibility});
+      this.setState({visibility: data.list[0].visibility});
       this.setState({loading: false});
+
+
+
+      //Getting and displaying the text for the upcoming five days of the week
+        var weekday = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat",];
+
+        //Function to get the correct integer for the index of the days array
+        function CheckDay(day){
+            if(day + d.getDay() > 6){
+                return day + d.getDay() - 7;
+            }
+            else{
+                return day + d.getDay();
+            }
+        }
+        var weekdays = [] ;
+        for(let i = 0; i<5; i++){
+                weekdays.push(weekday[CheckDay(i)]);
+            }
+        console.log(weekdays)
+
+     
+
+        this.setState({day1: weekdays[0]});
+        this.setState({day1temp: Math.round(data.list[0].main.feels_like-273.15)});
+        this.setState({day1img: ("http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png")});
+
+
+
+        this.setState({day2: weekdays[1]});
+        this.setState({day2temp: Math.round(data.list[1].main.feels_like-273.15)});
+        this.setState({day2img: ("http://openweathermap.org/img/wn/" + data.list[1].weather[0].icon + "@2x.png")});
+
+
+
+        this.setState({day3: weekdays[2]});
+        this.setState({day3temp: Math.round(data.list[2].main.feels_like-273.15)});
+        this.setState({day3img: ("http://openweathermap.org/img/wn/" + data.list[2].weather[0].icon + "@2x.png")});
+
+
+        this.setState({day4: weekdays[3]});
+        this.setState({day4temp: Math.round(data.list[3].main.feels_like-273.15)});
+        this.setState({day4img: ("http://openweathermap.org/img/wn/" + data.list[3].weather[0].icon + "@2x.png")});
+
+
+
+
+        
+        this.setState({day5: weekdays[4]});
+        this.setState({day5temp: Math.round(data.list[4].main.feels_like-273.15)});
+        this.setState({day5img: ("http://openweathermap.org/img/wn/" + data.list[4].weather[0].icon + "@2x.png")});
+
+
+
+
+
+
+
+
+
+
 
 
   }
@@ -161,14 +254,16 @@ export class Mainpage extends React.Component
                         </tr>
                     </table>
                 </div>
+
+              
                 
                 <div id="d2">
                     <div id="d2sContainer">
-                        <div className='d2s'><h4>Day1</h4><img src='http://openweathermap.org/img/w/01n.png'></img> <h4>Space</h4></div>
-                        <div className='d2s'><h4>Day2</h4><img src='http://openweathermap.org/img/w/01n.png'></img> <h4>Space</h4></div>
-                        <div className='d2s'><h4>Day3</h4><img src='http://openweathermap.org/img/w/01n.png'></img> <h4>Space</h4></div>
-                        <div className='d2s'><h4>Day4</h4><img src='http://openweathermap.org/img/w/01n.png'></img> <h4>Space</h4></div>
-                        <div className='d2s'><h4>Day5</h4><img src='http://openweathermap.org/img/w/01n.png'></img> <h4>Space</h4></div>
+                        <div className='d2s'><h4>{this.state.day1}</h4><img src={this.state.day1img}></img><h4>{this.state.day1temp}</h4></div>
+                        <div className='d2s'><h4>{this.state.day2}</h4><img src={this.state.day2img}></img><h4>{this.state.day2temp}</h4></div>
+                        <div className='d2s'><h4>{this.state.day3}</h4><img src={this.state.day3img}></img><h4>{this.state.day3temp}</h4></div>
+                        <div className='d2s'><h4>{this.state.day4}</h4><img src={this.state.day4img}></img><h4>{this.state.day4temp}</h4></div>
+                        <div className='d2s'><h4>{this.state.day5}</h4><img src={this.state.day5img}></img><h4>{this.state.day5temp}</h4></div>
                     </div>
                 </div>
 
